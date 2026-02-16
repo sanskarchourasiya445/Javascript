e66a63cb6342c1a7806864540656ee5b7c6ab3d1
Sanskar Chourasiya <sanskarchourasiya9179@gmail.com>
2026-02-16 17:59:38 +0530

    Add loop in loops.js
    Added examples of for, while, and do-while loops in JavaScript.

---
+++ loops.js
@@ -0,0 +1,22 @@
+// FOR LOOP - When you want to repeat code a specific number of times
+for (let i = 0; i < 3; i++) {
+    console.log("Repeat this 3 times");
+}
+
+// WHILE LOOP - When you want to repeat until a condition becomes false
+let isRunning = true;
+let count = 0;
+while (isRunning) {
+    count++;
+    console.log("Running... " + count);
+    if (count === 3) {
+        isRunning = false; // Stop after 3 times
+    }
+}
+
+// DO-WHILE LOOP - Same as while, but always runs at least once
+let x = 0;
+do {
+    console.log("This runs at least once");
+    x++;
+} while (x < 3);
