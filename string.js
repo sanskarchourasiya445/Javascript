1b12e164a0d7e0d61be16a0a48cf864e0ad70fb1
Sanskar Chourasiya <sanskarchourasiya9179@gmail.com>
2026-02-18 21:46:51 +0530

    Create string.js
    Removed comments and examples demonstrating various string concepts in JavaScript.

---
+++ string.js
@@ -0,0 +1,24 @@
+let singleQuote = 'Hello';
+let doubleQuote = "World";
+let templateLiteral = `Hello World`;
+
+console.log(singleQuote);
+console.log(doubleQuote);
+console.log(templateLiteral);
+
+let message = "JavaScript Strings";
+console.log("Length:", message.length);
+
+let text = "Programming";
+
+console.log("First Character:", text[0]);
+console.log("Last Character:", text[text.length - 1]);
+
+let str = "  Learn JavaScript Easily  ";
+
+console.log("Uppercase:", str.toUpperCase());
+console.log("Lowercase:", str.toLowerCase());
+console.log("Trim:", str.trim());
+console.log("Slice:", str.slice(2, 7));
+console.log("Substring:", str.substring(2, 7));
+
