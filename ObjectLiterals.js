0e9edc75a302903ab773fb2802e4ddfb2227f146
Sanskar Chourasiya <sanskarchourasiya9179@gmail.com>
2026-02-19 19:07:07 +0530

    Add ObjectLiterals.js

---
+++ ObjectLiterals.js
@@ -0,0 +1,18 @@
+// Object literals are used to store keyed collection & complex entities
+// properties => (key,value) pair
+// objects are collection of properties
+
+let student = {name : "Sanskar",
+               age : 20,
+               marks : 94,
+               city : "Indore"
+              };
+
+student.name
+//Sanskar
+student['marks']
+//94
+student['city']
+//Indore
+student
+//{name:'Sanskar',age:20,marks:94,city:'Indore'}
