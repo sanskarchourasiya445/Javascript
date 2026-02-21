27976b44e28877c5bec6b5b7221bd7cbc867f734
Sanskar Chourasiya <sanskarchourasiya9179@gmail.com>
2026-02-21 22:34:33 +0530

    Create arrays.js

---
+++ arrays.js
@@ -0,0 +1,33 @@
+
+let numbers = [10, 20, 30, 40, 50];
+console.log(numbers);
+// [10, 20, 30, 40, 50]
+console.log(numbers[0]);
+//10
+
+console.log(numbers[2]);
+//30
+numbers[1] = 25;
+console.log(numbers);
+//[10, 25, 30, 40, 50]
+console.log(numbers.length);
+// 5
+
+for (let i = 0; i < numbers.length; i++) {
+    console.log(numbers[i]);
+}
+/*
+10
+25
+30
+40
+50
+*/
+
+let fruits = ["Apple", "Banana", "Mango"];
+console.log(fruits);
+//["Apple", "Banana", "Mango"]
+
+let mixed = [1, "Hello", true, null];
+console.log(mixed);
+//[1, "Hello", true, null]
