51d4899a37efc259e532ff92d228d505d93a029b
Sanskar Chourasiya <sanskarchourasiya9179@gmail.com>
2026-02-21 22:38:43 +0530

    Array Fucntions

---
+++ arrFunc.js
@@ -0,0 +1,43 @@
+let numbers = [10, 20, 30, 40, 50];
+numbers.push(60);
+console.log(numbers);
+//[10, 25, 30, 40, 50, 60]
+numbers.pop();
+console.log(numbers);
+//[10, 25, 30, 40, 50]
+numbers.unshift(5);
+console.log(numbers);
+//[5, 10, 25, 30, 40, 50]
+numbers.shift();
+console.log(numbers);
+//[10, 25, 30, 40, 50]
+
+numbers.forEach(function(num) {
+    console.log(num);
+});
+/*
+10
+25
+30
+40
+50
+*/
+
+let doubled = numbers.map(function(num) {
+    return num * 2;
+});
+console.log(doubled);
+//[20, 50, 60, 80, 100]
+
+let greaterThan30 = numbers.filter(function(num) {
+    return num > 30;
+});
+console.log(greaterThan30);
+//[40, 50]
+console.log(numbers.includes(25));
+//true
+
+console.log(numbers.includes(100));
+//false
+console.log(numbers.indexOf(30));
+//2
