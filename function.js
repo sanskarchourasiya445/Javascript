ac50e477f807d514533b5f75a6e9ddb2a9577b50
Sanskar Chourasiya <sanskarchourasiya9179@gmail.com>
2026-02-23 23:17:47 +0530

    Create function.js

---
+++ function.js
@@ -0,0 +1,18 @@
+
+function add(a, b) {
+  return a + b;
+}
+
+
+function subtract(a, b) {
+  return a - b;
+}
+
+
+function isEven(num) {
+  return num % 2 === 0;
+}
+
+add(4,8)
+subtract(17,6)
+isEven(9)
