494ec9f414ffb51672061f9e9ad375420eed1be6
Sanskar Chourasiya <sanskarchourasiya9179@gmail.com>
2026-02-26 21:55:48 +0530

    Update ObjectLiterals.js

--- ObjectLiterals.js
+++ ObjectLiterals.js
@@ -18,3 +18,25 @@ studant.age
 //20
 student
 //{name:'Sanskar',age:20,marks:94,city:'Indore'}
+
+
+const user = {
+  username: "coder123",
+  role: "Developer",
+  login: function () {
+    return `${this.username} has logged in`;
+  }
+};
+
+const laptop = {
+  brand: "Dell",
+  specs: {
+    ram: "16GB",
+    storage: "512GB SSD",
+    processor: "Intel i5"
+  }
+};
+
+for (let key in student) {
+  console.log(key + " : " + student[key]);
+}
