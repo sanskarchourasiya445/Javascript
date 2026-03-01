24d09cd58c31840859c197a19bb93b46910db4bf
Sanskar Chourasiya <sanskarchourasiya9179@gmail.com>
2026-03-01 22:13:13 +0530

    Create math.js
    Add various Math functions and examples in math.js

---
+++ math.js
@@ -0,0 +1,38 @@
+
+console.log(Math.round(4.6));
+//5
+
+console.log(Math.ceil(4.9));
+//5
+
+console.log(Math.floor(4.9));
+//4
+
+console.log(Math.trunc(4.7));
+//4
+
+console.log(Math.sign(-4));
+//-1
+
+console.log(Math.pow(8, 2));
+//64
+
+console.log(Math.sqrt(64));
+//8
+
+console.log(Math.abs(-4.7));
+// 4.7
+
+console.log(Math.min(0,172,-8, -200));
+//-200
+
+console.log(Math.max(0, 150, 30, -200));
+//150
+
+
+// 🎲 Random Numbers
+console.log(Math.random());
+//Random number between 0 and 1
+
+console.log(Math.floor(Math.random() * 10) + 1);
+//Random integer between 1 and 10
